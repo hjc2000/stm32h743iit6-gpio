2b749953c0f4666586f1f3221e308572b4568737
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-30 00:18:10 +0800

    GpioPinPD8

--- private_src/PD/GpioPinPD8.cpp
+++ private_src/PD/GpioPinPD8.cpp
@@ -2,6 +2,51 @@
 #include <GpioPinOptions.h>
 #include <hal.h>
 
+void bsp::GpioPinPD8::Initialize(bsp::GpioPinOptions const &options)
+{
+    GPIO_InitTypeDef init = options;
+    if (options.WorkMode() == bsp::IGpioPinWorkMode::AlternateFunction)
+    {
+        if (options.AlternateFunction() == "fmc")
+        {
+            init.Alternate = GPIO_AF12_FMC;
+        }
+        else
+        {
+            throw std::invalid_argument{"不支持的 AlternateFunction"};
+        }
+    }
+
+    init.Pin = Pin();
+    HAL_GPIO_Init(Port(), &init);
+}
+
+bsp::GpioPinPD8 &bsp::GpioPinPD8::Instance()
+{
+    class Getter :
+        public base::SingletonGetter<GpioPinPD8>
+    {
+    public:
+        std::unique_ptr<GpioPinPD8> Create() override
+        {
+            return std::unique_ptr<GpioPinPD8>{new GpioPinPD8{}};
+        }
+
+        void Lock() override
+        {
+            DI_InterruptSwitch().DisableGlobalInterrupt();
+        }
+
+        void Unlock() override
+        {
+            DI_InterruptSwitch().EnableGlobalInterrupt();
+        }
+    };
+
+    Getter o;
+    return o.Instance();
+}
+
 GPIO_TypeDef *bsp::GpioPinPD8::Port()
 {
     return GPIOD;
@@ -17,6 +62,16 @@ std::string bsp::GpioPinPD8::PinName() const
     return "PD8";
 }
 
+base::IEnumerable<std::string> &bsp::GpioPinPD8::SupportedAlternateFunctions()
+{
+    return _supported_alternate_functions;
+}
+
+bool bsp::GpioPinPD8::IsOpen()
+{
+    return _is_open;
+}
+
 void bsp::GpioPinPD8::Open(bsp::IGpioPinOptions const &options)
 {
     if (_is_open)
@@ -24,12 +79,9 @@ void bsp::GpioPinPD8::Open(bsp::IGpioPinOptions const &options)
         throw std::runtime_error{"已经打开，要先关闭"};
     }
 
-    _is_open = true;
-
     __HAL_RCC_GPIOD_CLK_ENABLE();
-    GPIO_InitTypeDef init = static_cast<bsp::GpioPinOptions const &>(options);
-    init.Pin = Pin();
-    HAL_GPIO_Init(Port(), &init);
+    Initialize(static_cast<bsp::GpioPinOptions const &>(options));
+    _is_open = true;
 }
 
 void bsp::GpioPinPD8::Close()
