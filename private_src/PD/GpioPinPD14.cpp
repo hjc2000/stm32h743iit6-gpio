254d5c549d3ea3851d9c21d84d22a308a33536dd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-01 20:12:31 +0800

    GpioPinPD14

--- private_src/PD/GpioPinPD14.cpp
+++ private_src/PD/GpioPinPD14.cpp
@@ -2,6 +2,39 @@
 #include <GpioPinOptions.h>
 #include <hal.h>
 
+void bsp::GpioPinPD14::Initialize(bsp::GpioPinOptions const &options)
+{
+    GPIO_InitTypeDef init = options;
+    init.Pin = Pin();
+    HAL_GPIO_Init(Port(), &init);
+}
+
+bsp::GpioPinPD14 &bsp::GpioPinPD14::Instance()
+{
+    class Getter :
+        public base::SingletonGetter<GpioPinPD14>
+    {
+    public:
+        std::unique_ptr<GpioPinPD14> Create() override
+        {
+            return std::unique_ptr<GpioPinPD14>{new GpioPinPD14{}};
+        }
+
+        void Lock() override
+        {
+            DI_InterruptSwitch().DisableGlobalInterrupt();
+        }
+
+        void Unlock() override
+        {
+            DI_InterruptSwitch().EnableGlobalInterrupt();
+        }
+    };
+
+    Getter o;
+    return o.Instance();
+}
+
 GPIO_TypeDef *bsp::GpioPinPD14::Port()
 {
     return GPIOD;
@@ -17,6 +50,16 @@ std::string bsp::GpioPinPD14::PinName() const
     return "PD14";
 }
 
+base::IEnumerable<std::string> &bsp::GpioPinPD14::SupportedAlternateFunctions()
+{
+    return _supported_alternate_functions;
+}
+
+bool bsp::GpioPinPD14::IsOpen()
+{
+    return _is_open;
+}
+
 void bsp::GpioPinPD14::Open(bsp::IGpioPinOptions const &options)
 {
     if (_is_open)
@@ -24,12 +67,9 @@ void bsp::GpioPinPD14::Open(bsp::IGpioPinOptions const &options)
         throw std::runtime_error{"已经打开，要先关闭"};
     }
 
-    _is_open = true;
-
     __HAL_RCC_GPIOD_CLK_ENABLE();
-    GPIO_InitTypeDef init = static_cast<bsp::GpioPinOptions const &>(options);
-    init.Pin = Pin();
-    HAL_GPIO_Init(Port(), &init);
+    Initialize(static_cast<bsp::GpioPinOptions const &>(options));
+    _is_open = true;
 }
 
 void bsp::GpioPinPD14::Close()
