26825f8ecfffadf0982ea4ad5e5bbea02b9a5de1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-01 21:48:47 +0800

    GpioPinPE9

--- private_src/PE/GpioPinPE9.cpp
+++ private_src/PE/GpioPinPE9.cpp
@@ -2,6 +2,25 @@
 #include <GpioPinOptions.h>
 #include <hal.h>
 
+void bsp::GpioPinPE9::Initialize(bsp::GpioPinOptions const &options)
+{
+    GPIO_InitTypeDef init = options;
+    if (options.WorkMode() == bsp::IGpioPinWorkMode::AlternateFunction)
+    {
+        if (options.AlternateFunction() == "fmc")
+        {
+            init.Alternate = GPIO_AF12_FMC;
+        }
+        else
+        {
+            throw std::invalid_argument{"不支持的 AlternateFunction"};
+        }
+    }
+
+    init.Pin = Pin();
+    HAL_GPIO_Init(Port(), &init);
+}
+
 GPIO_TypeDef *bsp::GpioPinPE9::Port()
 {
     return GPIOE;
@@ -24,12 +43,9 @@ void bsp::GpioPinPE9::Open(bsp::IGpioPinOptions const &options)
         throw std::runtime_error{"已经打开，要先关闭"};
     }
 
-    _is_open = true;
-
     __HAL_RCC_GPIOE_CLK_ENABLE();
-    GPIO_InitTypeDef init = static_cast<bsp::GpioPinOptions const &>(options);
-    init.Pin = Pin();
-    HAL_GPIO_Init(Port(), &init);
+    Initialize(static_cast<bsp::GpioPinOptions const &>(options));
+    _is_open = true;
 }
 
 void bsp::GpioPinPE9::Close()
