a5768662861f7be39609ca9955aafa980380e2db
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-01 22:39:09 +0800

    GpioPinPF4

--- private_src/di/DI_GpioPinCollection.cpp
+++ private_src/di/DI_GpioPinCollection.cpp
@@ -60,6 +60,7 @@
 #include <PF/GpioPinPF1.h>
 #include <PF/GpioPinPF2.h>
 #include <PF/GpioPinPF3.h>
+#include <PF/GpioPinPF4.h>
 #pragma endregion
 
 #pragma region PG
@@ -141,6 +142,7 @@ namespace
             AddPin(bsp::GpioPinPF1::Instance());
             AddPin(bsp::GpioPinPF2::Instance());
             AddPin(bsp::GpioPinPF3::Instance());
+            AddPin(bsp::GpioPinPF4::Instance());
 #pragma endregion
 
 #pragma region PG
