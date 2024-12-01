bf67c207789803ad51e5fc531622e122bf5060a9
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-01 21:55:52 +0800

    GpioPinPE12

--- private_src/PE/GpioPinPE12.cpp
+++ private_src/PE/GpioPinPE12.cpp
@@ -2,6 +2,51 @@
 #include <GpioPinOptions.h>
 #include <hal.h>
 
+void bsp::GpioPinPE12::Initialize(bsp::GpioPinOptions const &options)
+{
+    GPIO_InitTypeDef init = options;
+    if (options.WorkMode() == bsp::IGpioPinWorkMode::AlternateFunction)
+    {
+        if (options.AlternateFunction() == "fmc")
+        {
+            init.Alternate = GPIO_AF12_FMC;
+        }
+        else
+        {
+            throw std::invalid_argument{"不支持的 AlternateFunction"};
+        }
+    }
+
+    init.Pin = Pin();
+    HAL_GPIO_Init(Port(), &init);
+}
+
+bsp::GpioPinPE12 &bsp::GpioPinPE12::Instance()
+{
+    class Getter :
+        public base::SingletonGetter<GpioPinPE12>
+    {
+    public:
+        std::unique_ptr<GpioPinPE12> Create() override
+        {
+            return std::unique_ptr<GpioPinPE12>{new GpioPinPE12{}};
+        }
+
+        void Lock() override
+        {
+            DI_InterruptSwitch().DisableGlobalInterrupt();
+        }
+
+        void Unlock() override
+        {
+            DI_InterruptSwitch().EnableGlobalInterrupt();
+        }
+    };
+
+    Getter o;
+    return o.Instance();
+}
+
 GPIO_TypeDef *bsp::GpioPinPE12::Port()
 {
     return GPIOE;
@@ -24,12 +69,9 @@ void bsp::GpioPinPE12::Open(bsp::IGpioPinOptions const &options)
         throw std::runtime_error{"已经打开，要先关闭"};
     }
 
-    _is_open = true;
-
     __HAL_RCC_GPIOE_CLK_ENABLE();
-    GPIO_InitTypeDef init = static_cast<bsp::GpioPinOptions const &>(options);
-    init.Pin = Pin();
-    HAL_GPIO_Init(Port(), &init);
+    Initialize(static_cast<bsp::GpioPinOptions const &>(options));
+    _is_open = true;
 }
 
 void bsp::GpioPinPE12::Close()
--- private_src/PE/GpioPinPE12.h
+++ private_src/PE/GpioPinPE12.h
@@ -2,6 +2,7 @@
 #include <base/container/Array.h>
 #include <base/RentedPtrFactory.h>
 #include <GpioPin.h>
+#include <GpioPinOptions.h>
 
 namespace bsp
 {
@@ -12,34 +13,15 @@ namespace bsp
         GpioPinPE12() = default;
 
         bool _is_open = false;
-        base::Array<std::string, 1> _supported_alternate_functions{"gpio"};
+
+        base::Array<std::string, 1> _supported_alternate_functions{
+            "fmc",
+        };
+
+        void Initialize(bsp::GpioPinOptions const &options);
 
     public:
-        static_function GpioPinPE12 &Instance()
-        {
-            class Getter :
-                public base::SingletonGetter<GpioPinPE12>
-            {
-            public:
-                std::unique_ptr<GpioPinPE12> Create() override
-                {
-                    return std::unique_ptr<GpioPinPE12>{new GpioPinPE12{}};
-                }
-
-                void Lock() override
-                {
-                    DI_InterruptSwitch().DisableGlobalInterrupt();
-                }
-
-                void Unlock() override
-                {
-                    DI_InterruptSwitch().EnableGlobalInterrupt();
-                }
-            };
-
-            Getter o;
-            return o.Instance();
-        }
+        static_function GpioPinPE12 &Instance();
 
         GPIO_TypeDef *Port() override;
         uint32_t Pin() override;
