450652b059eeb93bd27a7dbcabe8df93cef07389
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-01 20:05:15 +0800

    GpioPinPD9

--- private_src/PD/GpioPinPD9.cpp
+++ private_src/PD/GpioPinPD9.cpp
@@ -2,6 +2,39 @@
 #include <GpioPinOptions.h>
 #include <hal.h>
 
+void bsp::GpioPinPD9::Initialize(bsp::GpioPinOptions const &options)
+{
+    GPIO_InitTypeDef init = options;
+    init.Pin = Pin();
+    HAL_GPIO_Init(Port(), &init);
+}
+
+bsp::GpioPinPD9 &bsp::GpioPinPD9::Instance()
+{
+    class Getter :
+        public base::SingletonGetter<GpioPinPD9>
+    {
+    public:
+        std::unique_ptr<GpioPinPD9> Create() override
+        {
+            return std::unique_ptr<GpioPinPD9>{new GpioPinPD9{}};
+        }
+
+        void Lock() override
+        {
+            DI_InterruptSwitch().DisableGlobalInterrupt();
+        }
+
+        void Unlock() override
+        {
+            DI_InterruptSwitch().EnableGlobalInterrupt();
+        }
+    };
+
+    Getter o;
+    return o.Instance();
+}
+
 GPIO_TypeDef *bsp::GpioPinPD9::Port()
 {
     return GPIOD;
@@ -17,6 +50,16 @@ std::string bsp::GpioPinPD9::PinName() const
     return "PD9";
 }
 
+base::IEnumerable<std::string> &bsp::GpioPinPD9::SupportedAlternateFunctions()
+{
+    return _supported_alternate_functions;
+}
+
+bool bsp::GpioPinPD9::IsOpen()
+{
+    return _is_open;
+}
+
 void bsp::GpioPinPD9::Open(bsp::IGpioPinOptions const &options)
 {
     if (_is_open)
@@ -24,12 +67,9 @@ void bsp::GpioPinPD9::Open(bsp::IGpioPinOptions const &options)
         throw std::runtime_error{"已经打开，要先关闭"};
     }
 
-    _is_open = true;
-
     __HAL_RCC_GPIOD_CLK_ENABLE();
-    GPIO_InitTypeDef init = static_cast<bsp::GpioPinOptions const &>(options);
-    init.Pin = Pin();
-    HAL_GPIO_Init(Port(), &init);
+    Initialize(static_cast<bsp::GpioPinOptions const &>(options));
+    _is_open = true;
 }
 
 void bsp::GpioPinPD9::Close()
--- private_src/PD/GpioPinPD9.h
+++ private_src/PD/GpioPinPD9.h
@@ -2,6 +2,7 @@
 #include <base/container/Array.h>
 #include <base/RentedPtrFactory.h>
 #include <GpioPin.h>
+#include <GpioPinOptions.h>
 
 namespace bsp
 {
@@ -14,35 +15,13 @@ namespace bsp
         bool _is_open = false;
 
         base::Array<std::string, 1> _supported_alternate_functions{
-            "af_push_pull",
+            "fmc",
         };
 
+        void Initialize(bsp::GpioPinOptions const &options);
+
     public:
-        static_function GpioPinPD9 &Instance()
-        {
-            class Getter :
-                public base::SingletonGetter<GpioPinPD9>
-            {
-            public:
-                std::unique_ptr<GpioPinPD9> Create() override
-                {
-                    return std::unique_ptr<GpioPinPD9>{new GpioPinPD9{}};
-                }
-
-                void Lock() override
-                {
-                    DI_InterruptSwitch().DisableGlobalInterrupt();
-                }
-
-                void Unlock() override
-                {
-                    DI_InterruptSwitch().EnableGlobalInterrupt();
-                }
-            };
-
-            Getter o;
-            return o.Instance();
-        }
+        static_function GpioPinPD9 &Instance();
 
         GPIO_TypeDef *Port() override;
         uint32_t Pin() override;
@@ -56,18 +35,11 @@ namespace bsp
         /// @note 假设某个单片机的一个引脚的复用功能有：uart1, timer1_compare_output 等。
         /// 这种名称是与具体型号高度相关的，所以本库无法提供一个枚举来列举这些情况。
         /// @return
-        base::IEnumerable<std::string> &SupportedAlternateFunctions() override
-        {
-            return _supported_alternate_functions;
-        }
+        base::IEnumerable<std::string> &SupportedAlternateFunctions() override;
 
         /// @brief 指示此引脚是否已经打开。
         /// @return
-        bool IsOpen() override
-        {
-            return _is_open;
-        }
-
+        bool IsOpen() override;
         void Open(bsp::IGpioPinOptions const &options) override;
         void Close() override;
     };
